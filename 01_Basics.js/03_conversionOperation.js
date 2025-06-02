37320a6cecc76441913f59eabdd4c798e0e27973
Manish Kumawat <124272280+KumawatManish1@users.noreply.github.com>
2025-06-02 15:13:46 +0000

    operations done!

--- 01_Basics.js/03_conversionOperation.js
+++ 01_Basics.js/03_conversionOperation.js
@@ -22,3 +22,35 @@ let someNumber = 23
 let stringofsomeNumber  = String(someNumber)
 console.log(stringofsomeNumber)
 console.log(typeof stringofsomeNumber)
+
+
+
+/**************************************OPERATION***************************** */
+let someNum = 33
+let negNum = -someNum
+console.log(negNum)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Manish"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + "2")      //12
+console.log("1" + 2)      //12
+console.log("1" + 2 + 2)  //122
+console.log(1 + 2 + "2")  //32
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++               //prefix increment
+++gameCounter               //postfix increment
+console.log(gameCounter)
