587e2d70989c3ef172714b4d8d668e9b1d9281cb
Manish Kumawat <124272280+KumawatManish1@users.noreply.github.com>
2025-06-02 13:31:28 +0000

    dataTypes Done!

---
+++ 01_Basics.js/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //treats all JS code as newer version
+
+// alert(3+3) //we are using node js not browser that it why it is showing error
+
+console.log(3
+    +3
+)                 //code readability should be high
+
+
+let name = "Manish" //String
+let age = 23 //number
+let hasJob = false //Boolean
+
+//number 2 to the power 52
+//bigint
+//string = ""
+//boolean => true or false
+// null => standalone value
+//undefined
+//symbol => uniqueness
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
