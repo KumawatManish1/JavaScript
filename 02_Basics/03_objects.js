faf5a974f84ff9a7c2305807cb7962c877c96cc3
Manish Kumawat <124272280+KumawatManish1@users.noreply.github.com>
2025-06-25 05:31:47 +0000

    objects started

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,11 @@
+//singleton
+// Object.create
+
+//object literals
+const jsUser = {
+    name : "Manish",
+    age : 23,
+    location : "Mumbai",
+    email : "maish@google.com",
+    isloggedIn : false
+}
