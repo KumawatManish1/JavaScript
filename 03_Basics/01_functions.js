ab595a1501bc2d59d92a5a0b8199273b17cc54e8
Manish Kumawat <124272280+KumawatManish1@users.noreply.github.com>
2025-07-03 05:25:08 +0000

    functions done.

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -32,3 +32,37 @@ function loginUserMessage(username = "sam"){
 
 console.log(loginUserMessage())
 // console.log(loginUserMessage("hitesh"))
+
+
+
+function calculateCartPrice(val1, val2, ...num1){      //REST OPERATOR => ...num1
+    return num1;
+}
+
+console.log(calculateCartPrice(300,200,150,750))
+
+const user = {
+    userName: "Manish",
+    price : 199
+}
+
+//for object handling using functions
+
+function handleObject(anyObject){
+    console.log(`The username is ${anyObject.userName} and the price is ${anyObject.price}`)
+}
+
+handleObject(user)
+handleObject({
+    userName: "Sachin",
+    price: 399
+})
+
+//for array handling using Functions
+myNewArray = [100,300,256,444]
+
+function returnSecondValue(takeArray){
+    return takeArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,200,300,400]))
