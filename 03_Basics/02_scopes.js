86334bc9154b24a7909400d1a008259436d2c7ab
Manish Kumawat <124272280+KumawatManish1@users.noreply.github.com>
2025-07-14 05:31:23 +0000

    scope completed!

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,3 +9,41 @@ if(true){
 console.log(a)
 // console.log(b)
 console.log(c)
+
+
+function one(){
+    const userName = "Manish"
+
+    function two(){
+        const website = "Youtube"
+        console.log(userName)
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+
+if(true){
+    const userName = "Manish"
+    if(userName === "Manish"){
+        const website = " youtube"
+        console.log(userName + website)
+    }
+    // console.log(website)
+
+}
+// console.log(userName)
+
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+addtwo()
+const addtwo = function(num){
+    return num+2
+
+}
